7df238f2e2e1e79a91ea2ee8b9ffebe7faffe89d
YahefuYusufu <parguzatt@gmail.com>
2025-07-30 11:12:26 +0200

    GraphQL Resolvers

--- src/schema/resolvers/resolvers.js
+++ src/schema/resolvers/resolvers.js
@@ -1,11 +1,39 @@
-const { UserList } = require("../../../DummyData")
+const { UserList, MovieList } = require("../../../DummyData")
+const lodash = require("lodash")
 
 const resolvers = {
 	Query: {
-		users() {
+		users: () => {
 			return UserList
 		},
+		user: (_, args) => {
+			console.log("args.id:", args.id)
+			const id = args.id
+			const user = lodash.find(UserList, { id: id })
+			console.log("Found user:", user) // Debug log
+
+			return user
+		},
+
+		movies: () => {
+			return MovieList
+		},
+		movie: (parent, args) => {
+			const name = args.name
+			const movie = lodash.find(MovieList, { name: name })
+			return movie
+		},
 	},
+	User: {
+		favMovies: () => {
+			return lodash.filter(
+				MovieList,
+				(movie) => movie.yearOfPublication >= 2010
+			)
+		},
+	},
+
+	// No need for a User.friends resolver since friends are already complete objects in the data
 }
 
 module.exports = { resolvers }
--- src/schema/typeDefs/type-defs.js
+++ src/schema/typeDefs/type-defs.js
@@ -6,11 +6,36 @@ const typeDefs = gql`
 		name: String!
 		userName: String!
 		age: Int!
-		nationality: String!
+		nationality: Nationality!
+		friends: [User]
+		favMovies: [Movie]
+	}
+
+	type Movie {
+		id: ID!
+		name: String!
+		yearOfPublication: Int!
+		isInTheaters: Boolean!
 	}
 
 	type Query {
 		users: [User!]!
+		user(id: ID!): User!
+		movies: [Movie!]!
+		movie(name: String!): Movie!
+	}
+
+	enum Nationality {
+		UNITED_STATES
+		SPAIN
+		JAPAN
+		UNITED_KINGDOM
+		FRANCE
+		INDIA
+		BRAZIL
+		GERMANY
+		EGYPT
+		CANADA
 	}
 `
 module.exports = { typeDefs }
